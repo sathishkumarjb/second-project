ff68bdd7cd2330339d75984d8dbcc3d66301859b
sathishkumarjb <jsathish7299@gmail.com>
2023-07-08 12:20:29 +0530

    in python code add the user lucky number code

--- code.py
+++ code.py
@@ -1,3 +1,25 @@
-a = int(input("enter the number of a: "))
-b = int(input("enter the number of b: "))
-print ("the total value of a+b is " ,a+b)
+def calculate_lucky_number(name):
+    lucky_number = 0
+    
+    for char in name:
+        if char.isalpha():
+            char_value = ord(char.lower()) - ord('a') + 1
+            lucky_number += char_value
+    
+    while lucky_number > 9:
+        num_sum = 0
+        while lucky_number:
+            num_sum += lucky_number % 10
+            lucky_number //= 10
+        lucky_number = num_sum
+    
+    return lucky_number
+
+# Prompt the user to enter their name
+name = input("Enter your name: ")
+
+# Calculate the lucky number
+lucky_number = calculate_lucky_number(name)
+
+# Display the lucky number
+print("Your lucky number is:", lucky_number)
